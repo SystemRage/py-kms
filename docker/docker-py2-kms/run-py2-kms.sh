0c33c53b8c644e44528dd3cf96d0f4867e4dd5be
root <root@d1.phpchain.ru>
2019-09-13 12:35:38 +0400

    py3 docker updated

--- docker/docker-py2-kms/run-py2-kms.sh
+++ docker/docker-py2-kms/run-py2-kms.sh
@@ -1,12 +1,16 @@
+docker stop py2-kms
+docker rm py2-kms
 docker run -d --name py2-kms \
     -p 8080:8080 \
     -p 1688:1688 \
     -e IP=0.0.0.0 \
     -e PORT=1688 \
+    -e CLIENT_COUNT=30 \
     -e SQLITE=true \
-    -e HWID=random \
+    -e HWID=RANDOM \
     -e LOGLEVEL=INFO \
     -e LOGFILE=/var/log/py2-kms.log \
+    -e LOGSIZE=2 \
     -v /etc/localtime:/etc/localtime:ro \
     -v /var/log:/var/log:rw \
     --restart unless-stopped pykms/pykms:py2-kms
--- docker/docker-py3-kms/run-py3-kms.sh
+++ docker/docker-py3-kms/run-py3-kms.sh
@@ -1,12 +1,16 @@
+docker stop py3-kms
+docker rm py3-kms
 docker run -d --name py3-kms \
     -p 8080:8080 \
     -p 1688:1688 \
     -e IP=0.0.0.0 \
     -e PORT=1688 \
     -e SQLITE=true \
-    -e HWID=random \
+    -e HWID=RANDOM \
     -e LOGLEVEL=INFO \
     -e LOGFILE=/var/log/py3-kms.log \
+    -e LOGSIZE=2 \
     -v /etc/localtime:/etc/localtime:ro \
     -v /var/log:/var/log:rw \
     --restart unless-stopped pykms/pykms:py3-kms
+#    --restart unless-stopped ekonprof18/pykms:py3-kms
