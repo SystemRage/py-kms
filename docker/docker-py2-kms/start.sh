041df7f5ce9d02d2742fc355be5e76e770909be1
6ec123321 <artur@phpchain.ru>
2019-11-18 19:30:08 +0400

    Work with py2

--- docker/docker-py2-kms/start.sh
+++ docker/docker-py2-kms/start.sh
@@ -27,12 +27,12 @@ else
     then
       /bin/bash -c "/usr/bin/python pykms_Server.py ${IP} ${PORT} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} &"
       sleep 5
-      /usr/bin/python pykms_Client.py ${IP} ${PORT} -m Windows10 &
+      /usr/bin/python pykms_Client.py ${IP} ${PORT} -m Windows7 &
       /usr/bin/python /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/clients.db
     else
       /bin/bash -c "/usr/bin/python pykms_Server.py ${IP} ${PORT} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} -S ${LOGSIZE} &"
       sleep 5
-      /usr/bin/python pykms_Client.py ${IP} ${PORT} -m Windows10 &
+      /usr/bin/python pykms_Client.py ${IP} ${PORT} -m Windows7 &
       /usr/bin/python /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/clients.db
     fi
   else
@@ -40,12 +40,12 @@ else
     then
       /bin/bash -c "/usr/bin/python pykms_Server.py ${IP} ${PORT} -e ${EPID} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} &"
       sleep5
-      /usr/bin/python pykms_Client.py ${IP} ${PORT} -m Windows10 &
+      /usr/bin/python pykms_Client.py ${IP} ${PORT} -m Windows7 &
       /usr/bin/python /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/clients.db
     else
       /bin/sh -c "/usr/bin/python pykms_Server.py ${IP} ${PORT} -e ${EPID} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} -S ${LOGSIZE} &"
       sleep 5
-      /usr/bin/python pykms_Client.py ${IP} ${PORT} -m Windows10 &
+      /usr/bin/python pykms_Client.py ${IP} ${PORT} -m Windows7 &
       /usr/bin/python /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/clients.db
     fi
   fi
