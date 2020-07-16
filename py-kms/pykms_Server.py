11e32cccbd887a97baa13e1074a54d4d6c019905
Matteo ℱan <SystemRage@protonmail.com>
2020-07-16 22:59:44 +0200

    fix for #75

--- py-kms/pykms_Server.py
+++ py-kms/pykms_Server.py
@@ -194,7 +194,7 @@ def server_options():
         server_parser.add_argument("-l", "--lcid", action = "store", dest = srv_options['lcid']['des'], default = srv_options['lcid']['def'],
                                    help = srv_options['lcid']['help'], type = int)
         server_parser.add_argument("-c", "--client-count", action = "store", dest = srv_options['count']['des'] , default = srv_options['count']['def'],
-                                   help = srv_options['count']['help'], type = str)
+                                   help = srv_options['count']['help'], type = int)
         server_parser.add_argument("-a", "--activation-interval", action = "store", dest = srv_options['activation']['des'],
                                    default = srv_options['activation']['def'], help = srv_options['activation']['help'], type = int)
         server_parser.add_argument("-r", "--renewal-interval", action = "store", dest = srv_options['renewal']['des'],
@@ -204,7 +204,7 @@ def server_options():
         server_parser.add_argument("-w", "--hwid", action = "store", dest = srv_options['hwid']['des'], default = srv_options['hwid']['def'],
                                    help = srv_options['hwid']['help'], type = str)
         server_parser.add_argument("-t0", "--timeout-idle", action = "store", dest = srv_options['time0']['des'], default = srv_options['time0']['def'],
-                                   help = srv_options['time0']['help'], type = str)
+                                   help = srv_options['time0']['help'], type = int)
         server_parser.add_argument("-y", "--async-msg", action = "store_true", dest = srv_options['asyncmsg']['des'],
                                    default = srv_options['asyncmsg']['def'], help = srv_options['asyncmsg']['help'])
         server_parser.add_argument("-V", "--loglevel", action = "store", dest = srv_options['llevel']['des'], choices = srv_options['llevel']['choi'],
